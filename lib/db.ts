9c6244efcc4b655479f6ee1a78ffb8ae0d7b5d88
BrezzeT <veteroktm809@gmail.com>
2026-05-07 22:37:05 +0300

    update 0.5

--- lib/db.ts
+++ lib/db.ts
@@ -1,26 +1,31 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.DATABASE_URL;
-
-if (!MONGO_URI) {
-  throw new Error("Немає DATABASE_URL в .env.local");
-}
-
 let cached = (global as any).mongoose;
 if (!cached) {
   cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
 export const connectDB = async () => {
-  if (cached.conn) return cached.conn;
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGO_URI, {});
+  const MONGO_URI = process.env.DATABASE_URL;
+
+  if (!MONGO_URI) {
+    throw new Error("Немає DATABASE_URL в .env.local");
   }
+
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI, {
+      bufferCommands: false,
+    });
+  }
+
   try {
     cached.conn = await cached.promise;
   } catch (error) {
     cached.promise = null;
     throw error;
   }
+
   return cached.conn;
 };
